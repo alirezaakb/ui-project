47d455a358e92d912d701287c14e946a9692a145
Alireza Akbari <alireza10akbari@gmail.com>
2023-08-19 23:59:17 +0330

    add map section

--- js/map.js
+++ js/map.js
@@ -5,14 +5,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: ''
 }).addTo(map);
 
-
-var map = L.map('map').setView([51.505, -0.09], 13);
-var points = [
-    {coords: [51.5, -0.09], title: "نقطه 1"},
-    {coords: [51.51, -0.1], title: "نقطه 2"},
-    {coords: [51.49, -0.08], title: "نقطه 3"}
-];
-
 points.forEach(function (point) {
     L.marker(point.coords).addTo(map).bindPopup(point.title);
 });
