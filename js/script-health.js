f31584a1f40afe27ac80620f299c606ab0bb55d6
Alireza Akbari <alireza10akbari@gmail.com>
2023-08-29 02:34:22 +0330

    add date input

--- js/script-health.js
+++ js/script-health.js
@@ -8,3 +8,17 @@ chartBtn[1].addEventListener('click', function () {
     chartBtn[0].classList.remove('toggle-btn-active');
     chartBtn[1].classList.add('toggle-btn-active');
 });
+
+
+// persian date picker
+const options = {
+    forceFarsiDigits: true,
+    previousButtonIcon: 'img/caret-left.svg',
+    nextButtonIcon: 'img/caret-right.svg',
+    gotoToday: true,
+    sync: true,
+    markHolidays: true,
+    markToday: true
+}
+
+kamaDatepicker('date-input', options);
